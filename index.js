528d20d9cfffde4ef251d97b37455427e96b0428
Joanne Ndirangu <joannendirangu15@gmail.com>
2023-06-16 20:32:26 +0300

    Updated code-challenge

---
+++ index.js
@@ -0,0 +1,18 @@
+// Student Grade Generator
+
+const marks = prompt("Please enter a marks(between 0 and 100):");
+
+console.log(marks)
+
+if (marks>79) {
+    grade = "A";
+} else if (marks>=60 && marks<=79) {
+    grade = "B";
+} else if (marks>=49 && marks<=59) {
+    grade = "C";
+} else if (marks>=40 && marks<=49) {
+    grade = "D";
+} else {
+    grade = "E";
+}
+
