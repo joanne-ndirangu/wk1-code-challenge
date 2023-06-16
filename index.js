1a7bdfbc4b1b9aeca9418a99f7012c82393f4587
Joanne Ndirangu <joannendirangu15@gmail.com>
2023-06-17 00:31:37 +0300

    Updated code-challenge

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // Student Grade Generator
 
-const marks = prompt("Please enter a marks(between 0 and 100):");
+const marks = prompt("Please enter student marks(between 0 and 100):");
+
+const minLimit = 10;
+const maxLimit = 100;
 
 console.log(marks)
 
@@ -16,6 +19,58 @@ if (marks>79) {
     grade = "E";
 }
 
-console.log(`Grade: ${grade}`);
-document.write(`Student grade: ${grade}`)
+if (marks < 0 || marks > 100) {
+    alert("Invalid input! Please enter a number between 0 and 100.");
+  } else {
+    console.log(`Grade: ${grade}`);
+    document.write(`Student grade: ${grade}`)
+  }
 
+
+
+
+// Speed Detector
+
+function speedDetector(speed) {
+    if (speed<=70) {
+        return "Ok";
+    }
+}
+
+// const speedLimit = 70;
+// const speed1 = speed-speedLimit
+
+// const points = 0;
+
+// if (speed1<=5) {
+//     return (Points:1);
+// } else {
+//     for (let i = 0; i > 9; i++) {
+//         points = points + i;
+//       }
+//     console.log(speed1/5)
+//     console.log(`Points:${points}`);
+// }
+
+// if (points >= 12) {
+//     console.log(`License suspended`)
+// }
+
+// function calculateDemeritPoints(speed) {
+//     const speedLimit = 70;
+//     const kmPerDemeritPoint = 5;
+
+//     if (speed < speedLimit) {
+//       console.log("Ok");
+//     } else {
+//       const demeritPoints = Math.floor((speed - speedLimit) / kmPerDemeritPoint);
+//       console.log("Demerit Points:", demeritPoints);
+
+//       if (demeritPoints >= 12) {
+//         console.log("License suspended");
+//       }
+//     }
+//   }
+
+
+// Net Salary Calculator
