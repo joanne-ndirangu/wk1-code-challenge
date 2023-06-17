05975101d78edccfd5be9d5ac064b7abc4f4a419
Joanne Ndirangu <joannendirangu15@gmail.com>
2023-06-17 23:50:07 +0300

    Updated code-challenge

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // Student Grade Generator
 
-const marks = prompt("Please enter student marks(between 0 and 100):");
+const marks = prompt("Enter student marks(between 0 and 100):"); //prompts user to enter student marks when the webpage loads
 
-const minLimit = 10;
-const maxLimit = 100;
+const minLimit = 0;
+const maxLimit = 100;      //marks entered should be between 0 and 1
 
 console.log(marks)
 
-if (marks>79) {
+if (marks>79) {            //student marks grading system
     grade = "A";
 } else if (marks>=60 && marks<=79) {
     grade = "B";
@@ -20,34 +20,35 @@ if (marks>79) {
 }
 
 if (marks < 0 || marks > 100) {
-    alert("Invalid input! Please enter a number between 0 and 100.");
+    alert("Invalid input! Please enter a number between 0 and 100.");  //prompts user to enter a number between 0 and 100 if marks entered are not within limits set
   } else {
     console.log(`Grade: ${grade}`);
-    document.write(`Student grade: ${grade}`)
+    document.write(`Student grade: ${grade}`)   //outputs student grade
   }
 
 
 
 
-// // Speed Detector
+// Speed Detector
 
 function calculateDemeritPoints(speed) {
-   const speedLimit = 70;
-   let demeritPoints = 0;
+   const speedLimit = 70;       //any speed above this gets demerit points
+   let demeritPoints = 0;       //default points
 
-    if (speed <= speedLimit) {
+    if (speed <= speedLimit) {        //only if speed limit is not exceeded
         console.log(`Ok`)
         document.write(`Ok`)
     } else {
-        demeritPoints = (speed - speedLimit) / 5;
+        demeritPoints = Math.floor((speed - speedLimit) / 5);    //if speed limit is exceeded calculate demerit points
   }
 
         if (demeritPoints > 12) {
             console.log("License suspended")
             document.write("License suspended")
         } else {
+           Math.floor(demeritPoints);              //math.floor rounds decimal numbers down to the nearest whole number
             console.log(`Points:${demeritPoints}`)
-            document.write(`Points:${demeritPoints}`)
+            document.write(`Points:${demeritPoints}`)    //prints demerit points
         }
     }
 
@@ -55,3 +56,4 @@ function calculateDemeritPoints(speed) {
 
 
 // Net Salary Calculator
+
